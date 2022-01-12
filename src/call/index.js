85dad4616eaa5791a3dce2afabccfb792781a8e2
peakcool <tangliangcheng852@gmail.com>
2022-01-12 22:00:41 +0800

    feat: add call normal mock

---
+++ src/call/index.js
@@ -0,0 +1,43 @@
+Function.prototype.mockCall = function(o) {
+    // this 即调用的这个函数
+    
+    var context = o || window;
+    var args = [];
+
+    // 以上个例子为例，此时的arguments为：
+    // arguments = {
+    //      0: objFn,
+    //      1: 'kevin',
+    //      2: 18,
+    //      length: 3
+    // }
+    // 因为arguments是类数组对象，所以可以用for循环
+    for (var i = 1; i < arguments.length; i++) {
+        // args.push(arguments[i]); // es6
+        args.push(`arguments[${i}]`); // es5
+    }
+    
+    // 将方法绑定到对象上
+    context.fn = this;
+
+    // 执行方法
+    // context.fn(...args); // es6
+    eval(`context.fn(${args.join(',')})`); // es5
+
+    // 删除该属性
+    delete context.fn;
+}
+
+var obj = {
+    name: 'tom',
+}
+
+const objFn = function(age, colors) {
+    console.log(this.name);
+    console.log(age);
+    console.log(colors);
+}
+
+// this 指向了obj
+// objFn.call(obj, 24, ['white', 'yellow']) // tom
+objFn.mockCall(obj, 24, ['white', 'yellow']) // tom
